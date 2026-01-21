ad6c205cceffd75e9c11ca2e31c36e314561d251
LinkXP49 <Link.XP.49@gmail.com>
2026-01-20 20:14:21 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // Initialize Supabase
 const SUPABASE_URL = https://ekttgxbdhxedtpggurpj.supabase.co
-const SUPABASE_ANON_KEY = sb_secret_a1ah9xBAVbiXls5hDBeMAw_TUsBCNhP
-const supabase = supabase.createClient(https://ekttgxbdhxedtpggurpj.supabase.co, sb_secret_a1ah9xBAVbiXls5hDBeMAw_TUsBCNhP 
+const SUPABASE_ANON_KEY = sb_publishable_SHZvY63CAzzqrJHezW9-Lg_1TjFvHfZ
+const supabase = supabase.https://ekttgxbdhxedtpggurpj.supabase.co, sb_publishable_SHZvY63CAzzqrJHezW9-Lg_1TjFvHfZ
 // Example: Check if a user is logged in
 supabase.auth.onAuthStateChange((event, session) => {
     if(session?.user) {
